b30ecd5d539bbf81843361d3fa748281ec1ac0b9
dd017 <devyadav170908@gmail.com>
2024-08-15 10:17:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,84 @@ class ChessPiece:
     def __init__(self, color, name):
         self.color = color
         self.name = name
-    
+
     def __str__(self):
         return f"{self.color[0]}{self.name[0].upper()}"
 
+    def valid_moves(self, board, row, col):
+
+        pass
+
+class Pawn(ChessPiece):
+    def __init__(self, color):
+        super().__init__(color, 'pawn')
+
+    def valid_moves(self, board, row, col):
+
+        moves = []
+        direction = 1 if self.color == 'white' else -1
+
+
+        if 0 <= row + direction < 8 and board.board[row + direction][col] == ' ':
+            moves.append((row + direction, col))
+
+            if (self.color == 'white' and row == 6) or (self.color == 'black' and row == 1):
+                if board.board[row + 2 * direction][col] == ' ':
+                    moves.append((row + 2 * direction, col))
+
+
+        for dc in [-1, 1]:
+            if 0 <= col + dc < 8 and 0 <= row + direction < 8:
+                target = board.board[row + direction][col + dc]
+                if target != ' ' and target.color != self.color:
+                    moves.append((row + direction, col + dc))
+        return moves
+
+class Rook(ChessPiece):
+    def __init__(self, color):
+        super().__init__(color, 'rook')
+
+    def valid_moves(self, board, row, col):
+        moves = []
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        for dr, dc in directions:
+            r, c = row, col
+            while True:
+                r += dr
+                c += dc
+                if 0 <= r < 8 and 0 <= c < 8:
+                    target = board.board[r][c]
+                    if target == ' ':
+                        moves.append((r, c))
+                    elif target.color != self.color:
+                        moves.append((r, c))
+                        break
+                    else:
+                        break
+                else:
+                    break
+        return moves
 class ChessBoard:
     def __init__(self):
-        self.board = [[' ' for _ in range(8)] for _ in range(8)]
+        self.board = [[None for _ in range(8)] for _ in range(8)]
         self.setup_pieces()
 
     def setup_pieces(self):
         self.board[0] = [
-            ChessPiece('white', 'rook'), ChessPiece('white', 'knight'), ChessPiece('white', 'bishop'),
-            ChessPiece('white', 'queen'), ChessPiece('white', 'king'), ChessPiece('white', 'bishop'),
-            ChessPiece('white', 'knight'), ChessPiece('white', 'rook')
+            Rook('white'), Knight('white'), Bishop('white'),
+            Queen('white'), King('white'), Bishop('white'),
+            Knight('white'), Rook('white')
         ]
-        self.board[1] = [ChessPiece('white', 'pawn') for _ in range(8)]
+        self.board[1] = [Pawn('white') for _ in range(8)]
 
         self.board[7] = [
-            ChessPiece('black', 'rook'), ChessPiece('black', 'knight'), ChessPiece('black', 'bishop'),
-            ChessPiece('black', 'queen'), ChessPiece('black', 'king'), ChessPiece('black', 'bishop'),
-            ChessPiece('black', 'knight'), ChessPiece('black', 'rook')
+            Rook('black'), Knight('black'), Bishop('black'),
+            Queen('black'), King('black'), Bishop('black'),
+            Knight('black'), Rook('black')
         ]
-        self.board[6] = [ChessPiece('black', 'pawn') for _ in range(8)]
-    
+        self.board[6] = [Pawn('black') for _ in range(8)]
+
     def print_board(self):
         print("  a b c d e f g h")
         print(" +-----------------+")
@@ -33,31 +87,31 @@ class ChessBoard:
             print(f"{8 - row}|", end=" ")
             for col in range(8):
                 piece = self.board[row][col]
-                print(str(piece) if piece != ' ' else ' ', end=' ')
+                print(str(piece) if piece else ' ', end=' ')
             print(f"|{8 - row}")
         print(" +-----------------+")
         print("  a b c d e f g h")
+
 class ChessGame:
     def __init__(self):
         self.board = ChessBoard()
         self.current_player = 'white'
-    
+
     def move_piece(self, from_pos, to_pos):
         from_row, from_col = 8 - int(from_pos[1]), ord(from_pos[0]) - ord('a')
         to_row, to_col = 8 - int(to_pos[1]), ord(to_pos[0]) - ord('a')
-        
+
         piece = self.board.board[from_row][from_col]
-        
-        if piece == ' ' or piece.color != self.current_player:
+        if piece is None or piece.color != self.current_player:
             print("Invalid move: No piece of yours at this position.")
             return False
 
-        if self.board.board[to_row][to_col] != ' ' and self.board.board[to_row][to_col].color == self.current_player:
-            print("Invalid move: You can't capture your own piece.")
+        if (to_row, to_col) not in piece.valid_moves(self.board, from_row, from_col):
+            print("Invalid move: Piece cannot move to this position.")
             return False
 
         self.board.board[to_row][to_col] = piece
-        self.board.board[from_row][from_col] = ' '
+        self.board.board[from_row][from_col] = None
         self.current_player = 'black' if self.current_player == 'white' else 'white'
         return True
 
